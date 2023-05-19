2f06095e06df52b491a4d04b87d5c1fffbdaa82b
leonardo_goandete <leonardo_goandete>
2023-05-19 15:03:37 -0300

    Implementado uma possivel solução MinhaGen

--- src/br/com/ifrs/frete/dao/MinhaGen.java
+++ src/br/com/ifrs/frete/dao/MinhaGen.java
@@ -14,16 +14,24 @@ public class MinhaGen<T> {
     }
 
     public boolean adicionarLista(T objeto) {
-        return lista.add(objeto);
+        try {
+            return lista.add(objeto);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Erro ao inserir o objeto na lista: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Erro desconhecido durante o cadastro " + e.getMessage());
+        }
+        return false;
     }
+
 
     public T pesquisarListaPorNome(String nome) {
         for (T obj : lista) {
-            try {
+            if (obj instanceof Cliente) {
                 Cliente cli = (Cliente) obj;
-                if (cli.getNome().equalsIgnoreCase(nome)) return obj;
-            }catch (ClassCastException e){
-                System.out.println("Erro ao fazer cast do obj para classe Cliente durante a busca por Nome");
+                if (cli.getNome().equalsIgnoreCase(nome)) {
+                    return obj;
+                }
             }
         }
         return null;
@@ -31,25 +39,23 @@ public class MinhaGen<T> {
 
     public int excluirPorCpf(String cpf) {
         for (T obj : lista) {
-            try {
+            if (obj instanceof Cliente) {
                 Cliente cli = (Cliente) obj;
-                if (cli.getCpf().equalsIgnoreCase(cpf)){
+                if (cli.getCpf().equalsIgnoreCase(cpf)) {
                     lista.remove(obj);
                     return 1;
                 }
-            }catch (ClassCastException e){
-                System.out.println("Erro ao fazer cast do obj para classe Cliente durante a exclusao por CPF");
-            }
-            catch (UnsupportedOperationException e) {
-                System.out.println("Erro ao remover o objeto da lista: " + e.getMessage());
-            }catch (Exception e) {
-                System.out.println("Erro desconhecido durante a exclusão por CPF: " + e.getMessage());
             }
         }
         return 0;
     }
 
     public List<T> obterTodos() {
-        return lista;
+        try {
+            return lista;
+        } catch (Exception e) {
+            System.out.println("Erro desconhecido durante a listagem " + e.getMessage());
+        }
+        return null;
     }
 }
